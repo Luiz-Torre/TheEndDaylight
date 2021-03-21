13b43581248ac9ff8fc9bc31a2db107d58dd672d
Luiz Torre <luizmptorre@gmail.com>
2021-03-21 14:47:05 -0300

    Adicionando o fundo e a nave na fase 1

--- fase1.py
+++ fase1.py
@@ -1,15 +1,20 @@
 from PPlay.window import *
+from PPlay.gameimage import *
+from PPlay.sprite import *
 #Inicialização
 
 def fase(var_global):
     janela = Window(1280,768)
+    fundo= GameImage("fase1_imagens/fundo.png") 
     janela.set_title("Fase 1 - The End Daylight")
+    nave = Sprite("fase1_imagens/nave_sprite.png")
+    nave.set_position(janela.width/2,janela.height/2)
     teclado = Window.get_keyboard()
     primeira = 0
 
     while True:
-        janela.set_background_color((255,255,255))
-        janela.draw_text(f"Demonstração fase 1. Pressione ESC para retornar", 50, 550, size=10, color=(0,0,0), font_name="Times New Roman", bold=True, italic=False)
+        fundo.draw()  
+        nave.draw() 
 
         if(teclado.key_pressed("ESC")):
             var_global = 0
